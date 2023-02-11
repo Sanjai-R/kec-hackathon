9c0f9d54d01fd56ab277f04e25db64fd1965c5fe
Sanjai-R <samsanjai777@gmail.com>
2023-02-11 08:39:57 +0530

    fix(server): tracking added

--- server/controller/booking/tracking.controller.js
+++ server/controller/booking/tracking.controller.js
@@ -2,21 +2,21 @@ import trackingSchema from "../../models/tracking.schema.js";
 
 export const getTrackingId = async (id, type, requested_hall) => {
     let authorizers;
-    if (type == "CC") {
+    if (type == "Conventional Centre") {
         authorizers = ["HOD", "CCO", "Registrar", "Principal"]
-    } else if (type == "Maharaja") {
+    } else if (type == "Maharaja Auditorium") {
         authorizers = ["HOD", "CCO", "Registrar"]
     } else {
         authorizers = ["hall_incharge"]
     }
     
     const authorize_data = authorizers.map((authorizer) => ({ authorizer, tracking: "pending" }));
-
     const newTracking = {
         requested_by: id,
         tracking: authorize_data,
         requested_hall
     }
+    console.log(newTracking)
 
     const tracking = await trackingSchema(newTracking);
     const data = await tracking.save();
